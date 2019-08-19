aecacb0903ef7a3cbe4e696636c70a5a200edfd4
Kaviza90 <vieralaqueviera@gmail.com>
2019-08-18 23:12:35 -0500

    se crea el package-lock.json

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path');
+
+module.exports = filePath => path.extname(filePath) === '.md';
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,8 @@
 const mdLinks = require('../');
 
-
 describe('mdLinks', () => {
-
-  it('should...', () => {
+  it('should return true for a valid .md file', () => {
+    expect(mdLinks('../README.md')).toBe(true);
     console.log('FIX ME!');
   });
-
 });
