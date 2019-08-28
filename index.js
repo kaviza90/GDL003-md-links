fb1d1b21529e376f4c7e3748b7dd528ea0c68fa1
Kaviza90 <vieralaqueviera@gmail.com>
2019-08-27 23:39:12 -0500

    se hacen funciones de reconocer md, leer links e imprimirlos

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const path = require('path');
+const fs = require('fs');
+const program = require('commander');
+let mdLinks = {};
 
-module.exports = filePath => path.extname(filePath) === '.md';
+//mdlinks
+mdLinks.mdSearch = filePath => path.extname(filePath) === 'md';
+
+mdLinks.readFile = fs.readFile('README.md', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+  const found = data.match(regex);
+  const urls = [];
+  for (let i = 0; i < found.length; i++) {
+    urls.push(found[i]);
+  }
+  console.log(urls);
+
+  if (program.validate) {
+    requestHTTP(urls);
+  }
+  return urls;
+});
+
+module.exports = mdLinks;
+
+//module.exports = filePath => path.extname(filePath) === '.md';
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,7 +2,11 @@ const mdLinks = require('../');
 
 describe('mdLinks', () => {
   it('should return true for a valid .md file', () => {
-    expect(mdLinks('../README.md')).toBe(true);
-    console.log('FIX ME!');
+    expect(mdLinks, mdSearch('../README.md')).toBe(true);
+    //console.log('FIX ME!');
+  });
+
+  it('should return false for an invalid .md file', () => {
+    expect(mdLinks.mdSearch('../index.js')).toBe(false);
   });
 });
